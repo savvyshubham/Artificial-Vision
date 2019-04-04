d834abd432dd7116608f0e5e8cd814a1fb8c5a2c
apoorvgaur2610 <apoorvgaur10@gmail.com>
2019-04-05 01:06:56 +0530

    Importing cv2 and time libraries

---
+++ imagecapture.py
@@ -0,0 +1,11 @@
+import cv2
+import time
+#import matplotlib.pyplot as plt
+#cap = cv2.VideoCapture(0)
+#
+#sif cap.isOpened():
+#    ifcap,frame = cap.read() 
+#    cap.release()
+#    if ifcap and frame is not None:
+#        plt.imshow(frame)
+#        cv2.imwrite('img.jpg', frame)
