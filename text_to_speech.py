82f4433669e3dfe081dbf67176da2996780c0393
ValiantVaibhav <vaibhav5118402@gmail.com>
2019-04-05 01:56:09 +0530

    added code to set audio file name according to date

--- text_to_speech.py
+++ text_to_speech.py
@@ -5,7 +5,7 @@ import datetime
 text = "Glory be to the Father, and to the Son, and to the Holy Spirit, as it was in the beginning, is now, and ever shall be, world without end. Amen!"
 language = "en"
 now=datetime.datetime.now()
-# fn=now.strftime("C:/Users/HP/gTTS_audio_files/%Y_%m_%d_%H_%M_%S.mp3")
+fn=now.strftime("C:/Users/HP/gTTS_audio_files/%Y_%m_%d_%H_%M_%S.mp3")
 
 # resultobj = gTTS(text=text, lang=language, slow=False)
 
