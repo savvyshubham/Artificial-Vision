eb86b47269cfc1a14cc7d829240559e40d39f40c
apoorvgaur2610 <apoorvgaur10@gmail.com>
2019-04-05 01:51:08 +0530

    Video object released

--- imagecapture.py
+++ imagecapture.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 cap = cv2.VideoCapture(0)
 sif cap.isOpened():
     ifcap,frame = cap.read() 
-#   cap.release()
+        cap.release()
     if ifcap and frame is not None:
-#        plt.imshow(frame)
+        plt.imshow(frame)
         cv2.imwrite('img.jpg', frame)
