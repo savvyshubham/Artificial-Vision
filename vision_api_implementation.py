bf1a47bfc6c4e9986f3fbb8175b9c6fd6f7be715
savvyshubham <48221254+savvyshubham@users.noreply.github.com>
2019-04-05 02:22:05 +0530

    Added parameters for API request

--- vision_api_implementation.py
+++ vision_api_implementation.py
@@ -14,9 +14,9 @@ image_path = "E:/Siddharth/sdcard1/jesus52.jpg"
 
 # Reading the image
 image_data = open(image_path, "rb").read()
-#headers    = {'Ocp-Apim-Subscription-Key': subscription_key,
-#              'Content-Type': 'application/octet-stream'}
-#params     = {'visualFeatures': 'Categories,Description,Color'}
+headers    = {'Ocp-Apim-Subscription-Key': subscription_key,
+              'Content-Type': 'application/octet-stream'}
+params     = {'visualFeatures': 'Categories,Description,Color'}
 #response = requests.post(
 #    analyze_url, headers=headers, params=params, data=image_data)
 #response.raise_for_status()
