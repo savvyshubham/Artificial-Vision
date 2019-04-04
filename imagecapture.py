2da8340154b1a13573394297ba19a9feb96f0e0b
apoorvgaur2610 <apoorvgaur10@gmail.com>
2019-04-05 01:35:56 +0530

    Added Video Capture

--- imagecapture.py
+++ imagecapture.py
@@ -1,8 +1,7 @@
 import cv2
 import time
-#import matplotlib.pyplot as plt
-#cap = cv2.VideoCapture(0)
-#
+import matplotlib.pyplot as plt
+cap = cv2.VideoCapture(0)
 #sif cap.isOpened():
 #    ifcap,frame = cap.read() 
 #    cap.release()
