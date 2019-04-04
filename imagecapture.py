89b2c8608cf20111353c781027543aec8b150448
apoorvgaur2610 <apoorvgaur10@gmail.com>
2019-04-05 01:45:15 +0530

    Added Video Capture

--- imagecapture.py
+++ imagecapture.py
@@ -2,8 +2,8 @@ import cv2
 import time
 import matplotlib.pyplot as plt
 cap = cv2.VideoCapture(0)
-#sif cap.isOpened():
-#    ifcap,frame = cap.read() 
+sif cap.isOpened():
+    ifcap,frame = cap.read() 
 #    cap.release()
 #    if ifcap and frame is not None:
 #        plt.imshow(frame)
