67c0469cf4b52446986566478d2da02ddf86e9da
siddcool27 <siddharth.pandey_cs16@gla.ac.in>
2019-04-16 23:25:21 +0530

    Settings-Static Files

--- settings.py
+++ settings.py
@@ -116,12 +116,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-## Static files (CSS, JavaScript, Images)
-## https://docs.djangoproject.com/en/2.1/howto/static-files/
-#
-#STATIC_URL = '/static/'
-#
-#STATIC_DIR =os.path.join(BASE_DIR,'static')
-#STATICFILES_DIRS = [STATIC_DIR]
-#
-#
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_DIR =os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+
