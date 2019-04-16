e3ce1b696de442f8b9c3fab32136d857201e0718
siddcool27 <siddharth.pandey_cs16@gla.ac.in>
2019-04-16 23:22:34 +0530

    Settings-Databases

--- settings.py
+++ settings.py
@@ -69,20 +69,20 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'ArtificialVision_project.wsgi.application'
-#
-#
-## Database
-## https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-#
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-#
-#
+WSGI_APPLICATION = 'ArtificialVision_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 ## Password validation
 ## https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 #
