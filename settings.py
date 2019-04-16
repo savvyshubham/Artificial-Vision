ea8f4e891cc4b4aa4e3f955f666e7fa74a688436
siddcool27 <siddharth.pandey_cs16@gla.ac.in>
2019-04-16 23:24:27 +0530

    Settings-Password Internationalization

--- settings.py
+++ settings.py
@@ -102,20 +102,20 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-## Internationalization
-## https://docs.djangoproject.com/en/2.1/topics/i18n/
-#
-#LANGUAGE_CODE = 'en-us'
-#
-#TIME_ZONE = 'UTC'
-#
-#USE_I18N = True
-#
-#USE_L10N = True
-#
-#USE_TZ = True
-#
-#
+# Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
 ## Static files (CSS, JavaScript, Images)
 ## https://docs.djangoproject.com/en/2.1/howto/static-files/
 #
