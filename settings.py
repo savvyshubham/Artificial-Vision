0e7acbb2d6aa545ca1bf643d216086b4bc5b4b30
siddcool27 <siddharth.pandey_cs16@gla.ac.in>
2019-04-16 23:19:02 +0530

    Settings-Installed Apps

--- settings.py
+++ settings.py
@@ -29,18 +29,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-## Application definition
-#
-#INSTALLED_APPS = [
-#    'django.contrib.admin',
-#    'django.contrib.auth',
-#    'django.contrib.contenttypes',
-#    'django.contrib.sessions',
-#    'django.contrib.messages',
-#    'django.contrib.staticfiles',
-#    'ArtificialVision_app',
-#]
-#
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'ArtificialVision_app',
+]
+
 #MIDDLEWARE = [
 #    'django.middleware.security.SecurityMiddleware',
 #    'django.contrib.sessions.middleware.SessionMiddleware',
